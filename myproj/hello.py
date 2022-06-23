e5b891f7e41a042b80b57a8bb1591270750392d5
keshav <keshav.gk712@gmail.com>
2022-06-24 00:26:22 +0530

    second commit(now it can get the transcript of the video on its own)

--- myproj/hello.py
+++ myproj/hello.py
@@ -1,5 +1,7 @@
+from re import sub
 from flask import Flask , render_template , request
 from youtube_transcript_api import YouTubeTranscriptApi
+from flask import jsonify
 
 app=Flask(__name__)
 @app.route("/")
@@ -8,7 +10,13 @@ def hello_world():
 
 @app.route("/ytlink",methods=['POST'])
 def ytlink():
-    return  request.form['ytlink']
+    link=request.form['ytlink'] #to get the youtube video link from the main webpage
+    link=link[32:] #to get the youtube video id 
+    subtitle = YouTubeTranscriptApi.get_transcript(link,languages=['en']) #to get the transcript of the video 
+    return jsonify(subtitle) #this function cannot return a list so i have to make it in the form of a js object 
+
+
+
 if __name__=='__main__':
     app.debug=True
     app.run()
